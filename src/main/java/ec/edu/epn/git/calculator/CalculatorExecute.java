1c46bd384a921aece2edebca084524d8fa95ae19
jimmynow <jimmy.arias@epn.edu.ec>
2026-04-25 19:38:17 -0500

    Este es un cambio en la clase calculadora.

--- src/main/java/ec/edu/epn/git/calculator/CalculatorExecute.java
+++ src/main/java/ec/edu/epn/git/calculator/CalculatorExecute.java
@@ -10,5 +10,7 @@ public class CalculatorExecute {
 
         int subtraction = c.subtraction(7, 1);
         System.out.println("c.subtraction(7,1) = " + subtraction);
+
+        System.out.println("== End Calculator Execute ==");
     }
 }
